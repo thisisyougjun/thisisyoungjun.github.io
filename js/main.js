7770fad4b74667b6b02dbea4e3327df0bacf4d5e
thisisyoungjun <75524611+thisisyougjun@users.noreply.github.com>
2022-05-17 20:35:40 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// header.html include
+$(document).ready(function (){
+    $("#header_title").load("layout/header.html");
+})
+//footer include
+$(document).ready(function (){
+    $("#footer").load("layout/footer.html");
+})
+
+//header_title event
+const header_title=document.getElementsByClassName('header_title');
+
+console.log()
+
